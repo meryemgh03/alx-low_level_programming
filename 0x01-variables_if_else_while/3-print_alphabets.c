8c041ebeea8360fec384ca0403883b264c5e322e
meryemgh03 <meryemgh22@gmail.com>
2023-06-16 15:39:49 -0700

    lower and upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Lower and upper
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	char u;
+
+	 l = 'a';
+	 u = 'A';
+	 while (l <= 'z')
+	 {
+		 putchar(l);
+		 l++;
+	 }
+	 while (u <= 'z')
+	 {
+		 putchar(u);
+		 u++;
+	 }
+	 putchar('\n');
+	 return (0);
+}
