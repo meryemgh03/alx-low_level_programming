4b78accc42511d8822a68488ca8d2f0451d26396
meryemgh03 <meryemgh22@gmail.com>
2023-06-16 15:26:48 -0700

    the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The alphabet game
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
