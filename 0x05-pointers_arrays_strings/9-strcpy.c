cb31f9dd9b1864caf69c27c36d109916111312d7
meryemgh03 <meryemgh22@gmail.com>
2023-06-27 03:44:11 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int j;
 
-	while (src[j] != '\0')
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[j] = src[j];
-		j++;
 	}
 	dest[j++] = '\0';
 	return (dest);
